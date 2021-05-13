114aa1192c097c46dfa4ad9afb552fff6631b789
Fernando Bastida <bastida1102@gmail.com>
2021-05-12 19:27:22 -0500

    Imagen correcta

--- src/test/java/ProyectoClickin.java
+++ src/test/java/ProyectoClickin.java
@@ -2,13 +2,22 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.concurrent.TimeUnit;
+
+
+import java.util.Base64;
+import static org.apache.commons.io.IOUtils.toByteArray;
 
 public class ProyectoClickin {
     static AppiumDriver<MobileElement> driver;
@@ -24,6 +33,9 @@ public class ProyectoClickin {
         clickin.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
         clickin.setCapability("appPackage", "mx.alphacredit.clickink");
         clickin.setCapability("appActivity", "mx.alphacredit.clickink.Splash");
+
+        clickin.setCapability("sauceLabsImageInjectionEnabled", true);
+
         URL url = new URL("http://localhost:4723/wd/hub");
         driver = new AppiumDriver<MobileElement>(url, clickin);
         System.out.println("Application Started Successfully....");
@@ -31,12 +43,12 @@ public class ProyectoClickin {
     }
     @AfterClass
     public void closeMovil(){
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
     public void Flujo1() throws Exception {
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         MobileElement btn_TerminarTutorial = driver.findElement(By.id("mx.alphacredit.clickink:id/btn_TerminarTutorial"));
         btn_TerminarTutorial.click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -50,16 +62,37 @@ public class ProyectoClickin {
         permiso.click();
         MobileElement btnQR = driver.findElement(By.id("mx.alphacredit.clickink:id/img_qrCode"));
         btnQR.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         MobileElement btn_EmpezarFirma = driver.findElement(By.id("mx.alphacredit.clickink:id/btn_EmpezarFirma"));
         btn_EmpezarFirma.click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //imagen();
+
+        File file = new File("C:\\Users\\fer\\Desktop\\AppiumClickin\\resources\\imgQR_QA.png");
+        byte[] array = Files.readAllBytes(file.toPath());
+        String qrCodeImage = Base64.getEncoder().encodeToString(array);
+        ((JavascriptExecutor)driver).executeScript("sauce:inject-image=" + qrCodeImage);
+
+        System.out.println("codigo.." + qrCodeImage);
 
 
-        //sección de numeros de sms
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.MINUTES);
+
+
+
+
+                //sección de numeros de sms
         // mx.alphacredit.clickink:id/numberSMS222
         // sms 464852
     }
+/*public void  imagen() throws IOException {
+             // Read the file from the classpath and transform it to a base64 string
+            String qrCodeImage = Base64.getEncoder().encodeToString(
+            toByteArray(getClass().getResourceAsStream("qr-code.png")));
+            System.out.println(qrCodeImage);
 
+             // Provide the transformed image to the device
+            ((JavascriptExecutor)driver).executeScript("sauce:inject-image=" + qrCodeImage);
+}*/
 }
 
 
